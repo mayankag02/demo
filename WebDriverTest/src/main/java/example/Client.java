0f54dda7ea6424e7a5b4eefd9185f8dac991143b
agarwam <agarwam@MSNWQSS278.na.ds.ussco.com>
2019-04-08 13:10:28 -0500

    tst

--- WebDriverTest/src/main/java/example/Client.java
+++ WebDriverTest/src/main/java/example/Client.java
@@ -14,13 +14,14 @@ public class Client {
         String str = null;
         if (args[0].equalsIgnoreCase("Test1")) {
     		str = "C:\\Users\\agarwam\\eclipse-workspace\\WebDriverTest\\src\\main\\resources\\test1.xml"; //Use while running Jar
+    		System.out.println("Test1");
 //    		String str = "src/main/resources/test1.xml"; //Use while running as Java app
         }
-        else if (args[0].equalsIgnoreCase("Test1")) {
+        else if (args[0].equalsIgnoreCase("Test2")) {
     		str = "C:\\Users\\agarwam\\eclipse-workspace\\WebDriverTest\\src\\main\\resources\\test2.xml"; //Use while running Jar
 //    		String str = "src/main/resources/test2.xml"; //Use while running as Java app
+    		System.out.println("Test2");
         }
-        
         suites.add(str);
         testng.setTestSuites(suites);
         testng.run();
