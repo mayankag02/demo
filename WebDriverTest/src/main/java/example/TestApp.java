725ceac04a45596e3c0db4332d4ceaf91157accc
agarwam <agarwam@MSNWQSS278.na.ds.ussco.com>
2019-04-08 13:24:55 -0500

    rest

--- WebDriverTest/src/main/java/example/TestApp.java
+++ WebDriverTest/src/main/java/example/TestApp.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.testng.TestNG;
 import org.testng.collections.Lists;
 
-public class Client {
+public class TestApp {
 
 	public static void main(String[] args) throws Exception {
 		TestNG testng = new TestNG();
         List<String> suites = Lists.newArrayList();
-        
+//        String str = "src/main/resources/test1.xml";
         String str = null;
         if (args[0].equalsIgnoreCase("Test1")) {
     		str = "C:\\Users\\agarwam\\eclipse-workspace\\WebDriverTest\\src\\main\\resources\\test1.xml"; //Use while running Jar
