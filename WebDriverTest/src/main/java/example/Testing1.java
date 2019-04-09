ded3d834ee2343defc1a57f4dd940ef6afcae94e
agarwam <agarwam@MSNWQSS278.na.ds.ussco.com>
2019-04-09 12:54:41 -0500

    msg
    Signed-off-by: agarwam <agarwam@MSNWQSS278.na.ds.ussco.com>

--- WebDriverTest/src/main/java/example/Testing1.java
+++ WebDriverTest/src/main/java/example/Testing1.java
@@ -9,9 +9,9 @@ public class Testing1 {
 
 	@Test
 	public void test1() throws InterruptedException {
-		WebDriverService.getDriver().get("https://www.youtube.com/");
+		WebDriverService.getDriver().get("https://www.google.com/");
 		Thread.sleep(1000);
-		assertEquals(WebDriverService.getDriver().getTitle(), "YouTube");
-		Reporter.log("YouTube is running!");
+		assertEquals(WebDriverService.getDriver().getTitle(), "Google");
+		Reporter.log("Google is running!");
 	}
 }
